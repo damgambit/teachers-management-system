bf37cf53d6b34bf349883f718933d0961f41c57b
damgambit <damianogambitta@gmail.com>
2018-06-07 09:34:11 +0200

    fix2

--- resources/views/orari/print_orario_classe.blade.php
+++ resources/views/orari/print_orario_classe.blade.php
@@ -1,4 +1,24 @@
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        {{--CSRF Token--}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        {{--Title and Meta--}}
+        @meta
+
+        {{--Common App Styles--}}
+        {{ Html::style(mix('assets/app/css/app.css')) }}
+
+        {{--Styles--}}
+        @yield('styles')
+
+        {{--Head--}}
+        @yield('head')
+
+</head>
     <div class="container">
 
         <div class="col-lg-12">
