283813739fee62ef030e93f232b080b1c5a5ad5b
damgambit <damianogambitta@gmail.com>
2018-06-07 09:31:43 +0200

    fix

--- resources/views/orari/print_orario_classe.blade.php
+++ resources/views/orari/print_orario_classe.blade.php
@@ -78,3 +78,14 @@
 
     </div>
     
+
+@section('scripts')
+    @parent
+    {{ Html::script(mix('assets/admin/js/dashboard.js')) }}
+@endsection
+
+@section('styles')
+    @parent
+    {{ Html::style(mix('assets/admin/css/dashboard.css')) }}
+@endsection
+
